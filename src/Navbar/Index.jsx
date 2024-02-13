6dcc2064a4f378f53a37cedca91a2cc1bc3bc995
mehakpal-d <mehakpal@diveroid.com>
2024-02-13 11:15:30 +0530

    CSS Navbar Concept issue

--- src/Navbar/Index.jsx
+++ src/Navbar/Index.jsx
@@ -4,14 +4,35 @@ const Navbar = () => {
         <div className="flex space-x-2">
         <div><div className = "text-left font-[poppins]">Blog-Fest</div></div>
         <div>
-        <div><img className ="h-5 inline"src = "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/comment-blog-icon.png" alt="Blog-Icon"/></div>
+        <div><img className ="h-5 inline cursor-pointer"src = "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/comment-blog-icon.png" alt="Blog-Icon"/></div>
         </div>
         </div>
-      <div className="text-3xl">
+      <div className="text-3xl ">
+        <div className="flex-col">
         <div className="space-y-2">
           <div className="w-8 h-0.5 bg-white"></div>
           <div className="w-8 h-0.5 bg-white"></div>
           <div className="w-8 h-0.5 bg-white"></div>
+        </div>
+        <div>
+        <ul className="flex">
+            <li>
+                <a href="#" className="text-xl peer-hover:text-cyan-500 duration-500">Home</a>
+            </li>
+            <li>
+                <a href="#" className="text-xl peer-hover:text-cyan-500 duration-500">About</a>
+            </li>
+            <li>
+                <a href="#" className="text-xl peer-hover:text-cyan-500 duration-500">Press</a>
+            </li>
+            <li>
+                <a href="#" className="text-xl peer-hover:text-cyan-500 duration-500">Contact</a>
+            </li>
+            <li>
+                <a href="#" className="text-xl peer-hover:text-cyan-500 duration-500">Login</a>
+            </li>
+        </ul>
+        </div>
         </div>
       </div>
     </div>
